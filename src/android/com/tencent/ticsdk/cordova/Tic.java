dc68f5a47d7aaa6ded7d0e52ef8c210a6aaed7ee
Kenny <menhal@qq.com>
2019-07-03 14:39:55 +0800

    android版回退

--- src/android/com/tencent/ticsdk/cordova/Tic.java
+++ src/android/com/tencent/ticsdk/cordova/Tic.java
@@ -7,25 +7,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.nfc.Tag;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.text.Layout;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import com.tencent.TIMMessage;
 import com.tencent.TIMUserStatusListener;
@@ -58,34 +47,14 @@ import java.util.List;
 
 
 
-public class Tic extends CordovaPlugin implements IClassEventListener, TicMessageListener, TIMUserStatusListener {
+public class Tic extends CordovaPlugin implements IClassEventListener, IClassroomIMListener, TIMUserStatusListener {
 
     //    CallbackContext callbackContext;
     final static int REQUEST_CODE = 1;
 
-    private String showTeacherName = "管理员";
-
     private boolean isRunning = false;
-    private boolean isShowStudents = true;  // 是否折叠学生视图
-
     private Dialog mainDialog = null;
-    private Button handBtn = null;  // 举手按钮
-    private Button collapseBtn = null; // 折叠按钮
-    private View toggleBtn = null; // 交换按钮
-    private ViewGroup layoutLeftBottom = null; // 学生列表Layout
-    private ViewGroup layoutRightBottom = null; // 右侧聊天窗
-    private View closeBtn = null; // 退出按钮
-    private ViewGroup layoutLeftTopWrapper = null; // 组视频/白板区域的container
-    private View leftBtn = null;  // 左滑按钮
-    private View rightBtn = null; // 右滑按钮
-    private HorizontalScrollView av_root_scroll = null; // 学生列表横向滚动视图
-    private ViewGroup av_root_container = null;  // 学生列表container
-    private ILiveRootView av_teacher_view = null; // 教师视频
-    private ILiveRootView av_self_view = null; // 教师视频
-    private WhiteboardView whiteboardview = null;  // 白板视图
-    private LinearLayout MessageContainer = null; // 聊天内容
-    private ScrollView chatScrollView = null; // 聊天滚动区
-    private EditText editText = null; // 输入框
+    private Button handBtn = null;
 
     private int roomId = 0;
     private String teacherId = "";
@@ -93,25 +62,19 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
     private String userSig = "";
     private String role = "";
     private CallbackContext callbackContext = null;
-    private TicMessageHandler messageHandler = null;
 
-    @Override
-    protected void pluginInitialize() {
-        messageHandler = new TicMessageHandler();
-        messageHandler.setMessageListener(this);
 
+//    @Override
+//    protected void pluginInitialize() {
 //        TICSDK.getInstance().initSDK(cordova.getActivity(), sdkappid);
 //        checkCameraAndMicPermission();
-    }
+//    }
 
 
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
 
         this.callbackContext = callbackContext;
-
-//        initLayout();
-//        return true;
 
         if ("init".equals(action)) {
             int sdkappid = args.optInt(0);
@@ -180,7 +143,7 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
 
 
         ClassEventObservable.getInstance().deleteObserver(this);
-        ClassroomIMObservable.getInstance().deleteObserver(messageHandler);
+        ClassroomIMObservable.getInstance().deleteObserver(this);
 
         isRunning = false;
     }
@@ -236,37 +199,21 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
         });
     }
 
-
-    // 初始化控件和布局
-    private void initLayout(){
+    private void onJoinRoomSuccess(){
         cordova.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        sendPluginResult();
+
+
+        ClassEventObservable.getInstance().addObserver(this);
+        ClassroomIMObservable.getInstance().addObserver(this);
+
 
         Activity activity = cordova.getActivity();
 
         // 添加主对话框
         mainDialog = new Dialog(activity, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
-        mainDialog.setContentView(getIdentifier("tic_main2", "layout"));
+        mainDialog.setContentView(getIdentifier("tic_main", "layout"));
 
-
-        handBtn = (Button) findViewById("handBtn");  // 举手按钮
-        collapseBtn = (Button) findViewById("collapseBtn"); // 折叠按钮
-        toggleBtn = findViewById("toggleBtn"); // 交换按钮
-        layoutLeftBottom = (ViewGroup) findViewById("LayoutLeftBottom"); // 学生列表Layout
-        layoutRightBottom = (ViewGroup) findViewById("LayoutRightBottom"); // 右侧聊天窗
-        closeBtn = findViewById("closeBtn"); // 退出按钮
-        layoutLeftTopWrapper = (ViewGroup) findViewById("LayoutLeftTopWrapper"); // 组视频/白板区域的container
-        leftBtn = findViewById("leftBtn");  // 左滑按钮
-        rightBtn = findViewById("rightBtn"); // 右滑按钮
-        av_root_scroll = (HorizontalScrollView) findViewById("av_root_scroll"); // 学生列表横向滚动视图
-        av_root_container = (ViewGroup) findViewById("av_root_container");  // 学生列表container
-        av_teacher_view = (ILiveRootView) findViewById("av_teacher_view"); // 教师视频
-        av_self_view = (ILiveRootView) findViewById("av_self_view"); // 本人视频
-        whiteboardview = (WhiteboardView) findViewById("whiteboardview");  // 白板视图
-        MessageContainer = (LinearLayout) findViewById("MessageContainer");
-        chatScrollView = (ScrollView) findViewById("ChatScrollView"); // 聊天滚动区
-        editText = (EditText) findViewById("editText"); // 输入框
-
-        // 关闭对话框事件
         mainDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialog) {
@@ -274,48 +221,26 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
             }
         });
 
-        // 收起按钮
-        collapseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isShowStudents = !isShowStudents;
-                resetLayout();
-            }
-        });
+        mainDialog.show();
 
-        // 交换按钮
-        toggleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleScreen();
-            }
-        });
 
-        // 关闭按钮点击
-        closeBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                mainDialog.cancel();
-            }
-        });
+        // 教师屏幕和各学员屏幕
+        ILiveRootView teacherVideo = (ILiveRootView) findViewById("av_root_view");
+        teacherVideo.initViews();
+        teacherVideo.render(teacherId, 1);
 
-        // 添加左划按钮
-        leftBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                av_root_scroll.arrowScroll(View.FOCUS_LEFT);
-            }
-        });
 
-        // 添加右划按钮
-        rightBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                av_root_scroll.arrowScroll(View.FOCUS_RIGHT);
-            }
-        });
+        LinearLayout teacherVideoContainer = (LinearLayout) findViewById("av_root_view_container");
+
+        if(Build.VERSION.SDK_INT >= 21){
+            teacherVideoContainer.setClipToOutline(true);
+        }
+
+        createMemberVideos();
+
 
         // 添加举手按钮
+        handBtn = (Button) findViewById("button");
         handBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -323,96 +248,39 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
             }
         });
 
-        // 聊天输入
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-
+        // 添加关闭按钮
+        Button closeBtn = (Button) findViewById("closeBtn");
+        closeBtn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
-                        actionId == EditorInfo.IME_ACTION_DONE ||
-                        event != null &&
-                                event.getAction() == KeyEvent.ACTION_DOWN &&
-                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    if (event == null || !event.isShiftPressed()) {
-                        // the user is done typing.
-                        String message = editText.getText().toString();
-                        messageHandler.sendBroadcast(message);
-                        showMessage(userId, message);
-                        editText.setText("");
-                        return false; // consume.
-                    }
-                }
-                return false;
+            public void onClick(View v){
+                mainDialog.cancel();
             }
         });
 
-        mainDialog.show();
-    }
 
+        final HorizontalScrollView avRootScroll = (HorizontalScrollView) findViewById("av_root_scroll");
 
-    // 切换布局
-    private void resetLayout(){
+        // 添加左划按钮
+        Button leftBtn = (Button) findViewById("leftBtn");
+        leftBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                avRootScroll.arrowScroll(View.FOCUS_LEFT);
+            }
+        });
 
-        if(isShowStudents){
-
-            layoutLeftBottom.setVisibility(View.VISIBLE);
-            layoutRightBottom.setVisibility(View.VISIBLE);
-            av_self_view.setVisibility(View.GONE);
-            collapseBtn.setText("收起");
-            stopSelfVideo();
-            createMemberVideos();
-
-        } else {
-
-            layoutLeftBottom.setVisibility(View.GONE);
-            layoutRightBottom.setVisibility(View.GONE);
-            av_self_view.setVisibility(View.VISIBLE);
-            collapseBtn.setText("展开");
-            clearMemberVideos();
-            renderSelfVideo();
-        }
-    }
-
-
-    // 切换白板和视频
-    private void toggleScreen(){
-
-        ViewGroup postion1 = (ViewGroup) av_teacher_view.getParent();
-        ViewGroup postion2 = (ViewGroup) whiteboardview.getParent();
-
-        postion1.removeView(av_teacher_view);
-        postion2.removeView(whiteboardview);
-
-        postion1.addView(whiteboardview, 0);
-        postion2.addView(av_teacher_view, 0);
-
-//        if(isShowStudents){
-//            clearMemberVideos();
-//            createMemberVideos();
-//        }
-    }
-
-    private void onJoinRoomSuccess(){
-        cordova.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
-        sendPluginResult();
-
-
-        ClassEventObservable.getInstance().addObserver(this);
-        ClassroomIMObservable.getInstance().addObserver(messageHandler);
-        messageHandler.setTeacherId(teacherId);
-
-        initLayout();
-
-        // 教师屏幕和各学员屏幕
-        av_teacher_view.initViews();
-        av_teacher_view.render(teacherId, 1);
+        // 添加右划按钮
+        Button rightBtn = (Button) findViewById("rightBtn");
+        rightBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                avRootScroll.arrowScroll(View.FOCUS_RIGHT);
+            }
+        });
 
         // 禁止学员操作白板
+        WhiteboardView whiteboardview = (WhiteboardView) findViewById("whiteboardview");
         whiteboardview.setWhiteboardEnable(false);
-
-        // 各学员视图
-//        createMemberVideos();
-        resetLayout();
 
         log("加入房间成功");
     }
@@ -423,41 +291,16 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
 
 
     private void createMemberVideos(){
+        renderUserVideo(userId);
+
         ContextEngine contextEngine = ILiveSDK.getInstance().getContextEngine();
         List<String> userList = contextEngine.getVideoUserList(CommonConstants.Const_VideoType_Camera);
 
-        renderUserVideo(userId);
-
-        for (String currentId : userList) {
-            if(currentId.equals(teacherId)) continue;
-            if(currentId.equals(userId)) continue;
-            renderUserVideo(currentId);
+        for (String userId : userList) {
+            if(userId.equals(teacherId)) continue;
+            if(userId.equals(userId)) continue;
+            renderUserVideo(userId);
         }
-    }
-
-    private void clearMemberVideos(){
-        LinearLayout layout = (LinearLayout) findViewById("av_root_container");
-
-        for (int i = 0; i < layout.getChildCount(); i++) {
-            ILiveRootView video = (ILiveRootView) layout.getChildAt(i);
-            video.closeVideo();
-
-            layout.post(new Runnable(){
-                public void run(){
-                    layout.removeView(video);
-                }
-            });
-        }
-    }
-
-
-    private void renderSelfVideo(){
-        av_self_view.initViews();
-        av_self_view.render(userId, 1);
-    }
-
-    private void stopSelfVideo(){
-        av_self_view.closeVideo();
     }
 
 
@@ -468,6 +311,10 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
         videoView.initViews();
         videoView.render(userId, 1);
         videoView.setDeviceRotation(180);
+//        videoView.setBackground(getIdentifier("layout_bg", "drawable"));
+//        videoView.setClipToOutline(true);
+
+
         layout.addView(videoView);
 
         layout.post(new Runnable(){
@@ -480,20 +327,14 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
         });
     }
 
+
+
     private void removeUserVideo(String userId){
         LinearLayout layout = (LinearLayout) findViewById("av_root_container");
 
         for (int i = 0; i < layout.getChildCount(); i++) {
             ILiveRootView video = (ILiveRootView) layout.getChildAt(i);
-            if(video.getIdentifier().equals(userId)) {
-                video.closeVideo();
-
-                layout.post(new Runnable(){
-                    public void run(){
-                        layout.removeView(video);
-                    }
-                });
-            }
+            if(video.getIdentifier().equals(userId)) layout.removeView(video);
         }
 
     }
@@ -565,7 +406,6 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
             if(newUserId.equals(teacherId)) continue;
             if(newUserId.equals(userId)) continue;
             renderUserVideo(newUserId);
-            showMessage("群消息提示", newUserId+"进入房间");
         }
     }
 
@@ -577,7 +417,6 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
             if(quitUserId.equals(teacherId)) continue;
             if(quitUserId.equals(userId)) continue;
             removeUserVideo(quitUserId);
-            showMessage("群消息提示", quitUserId+"退出了房间");
         }
     }
 
@@ -586,38 +425,53 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
 
     }
 
-    // 接收到远程指令
+
     @Override
-    public void onCommand(String text) {
-        if(text.equals("TIMCustomHandReplyYes")){
-            messageHandler.sendCommand("TIMCustomHandRecOpenOk");
+    public void onRecvTextMsg(int type, String userId, String message) {
+        if(userId.equals(teacherId)) onTeacherC2CMessage(message);
+    }
+
+    @Override
+    public void onRecvCustomMsg(int type, String s, byte[] bytes) {
+//        LinkedList<IClassroomIMListener> tmpList = new LinkedList<>(listObservers);
+//        for (IClassroomIMListener listener : tmpList) {
+//            listener.onRecvCustomMsg(type, s, bytes);
+//        }
+    }
+
+    @Override
+    public void onRecvMessage(TIMMessage message) {
+//        LinkedList<IClassroomIMListener> tmpList = new LinkedList<>(listObservers);
+//        for (IClassroomIMListener listener : tmpList) {
+//            listener.onRecvMessage(message);
+//        }
+    }
+
+
+    // 接收到老师的消息
+    private void onTeacherC2CMessage(String message){
+        if(message.equals("TIMCustomHandReplyYes")){
+            sendC2CMessageToTeacher("TIMCustomHandRecOpenOk");
             setMic(true);
             handBtn.setText("正在发言");
-            showMessage(showTeacherName, "你正在发言");
-        } else if(text.equals("TIMCustomHandReplyNo")){
-            messageHandler.sendCommand("TIMCustomHandRecCloseOk");
+        } else if(message.equals("TIMCustomHandReplyNo")){
+            sendC2CMessageToTeacher("TIMCustomHandRecCloseOk");
             setMic(false);
             handBtn.setText("我要发言");
-            showMessage(showTeacherName, "你已结束发言");
         }
     }
 
-    // 接收到群聊消息
-    @Override
-    public void onBroadcast(String fromUserId, String text) {
-        showMessage(fromUserId, text);
-    }
-
-    // 在对话框显示聊天信息
-    private void showMessage(String fromUserId, String text){
-        String fromUserText = fromUserId.equals(teacherId) ? showTeacherName : fromUserId;
-        TicMessageView messageView = new TicMessageView(cordova.getContext(), fromUserText, text);
-        MessageContainer.addView(messageView);
-
-        MessageContainer.post(new Runnable() {
+    // 发送消息给老师
+    private void sendC2CMessageToTeacher(String message){
+        TICManager.getInstance().sendTextMessage(teacherId, message, new ILiveCallBack() {
             @Override
-            public void run() {
-                chatScrollView.scrollTo(0, 999999);
+            public void onSuccess(Object data) {
+                log("发送消息成功");
+            }
+
+            @Override
+            public void onError(String module, int errCode, String errMsg) {
+                sendErrorMessage(errCode, errMsg);
             }
         });
     }
@@ -627,19 +481,17 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
         if(!"我要发言".equals(handBtn.getText())) return;
 
         handBtn.setText("等待老师同意...");
-        messageHandler.sendCommand("TIMCustomHand");
+        sendC2CMessageToTeacher("TIMCustomHand");
     }
 
     @Override
     public void onForceOffline() {
-        LOG.e("Tic", "onForceOffline");
-        if(mainDialog != null) mainDialog.cancel();
+
     }
 
     @Override
     public void onUserSigExpired() {
-        LOG.e("Tic", "onUserSigExpired");
-        if(mainDialog != null) mainDialog.cancel();
+
     }
 
 
@@ -694,6 +546,4 @@ public class Tic extends CordovaPlugin implements IClassEventListener, TicMessag
     private void log(String msg){
         Log.e("Tic", msg);
     }
-
-
 }
--- src/android/com/tencent/ticsdk/cordova/TicMessageListener.java
+++
@@ -1,6 +0,0 @@
-package com.tencent.ticsdk.cordova;
-
-public interface TicMessageListener {
-    void onCommand(String text);
-    void onBroadcast(String fromUserId, String text);
-}
